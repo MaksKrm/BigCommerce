44768371d0d1d0fe7a85aaf3c8986356b5376996
vasyliev_maks <maks.ukraine13@gmail.com>
2021-01-18 10:38:31 +0200

    test development

--- resources/views/bigCommerce/components/blocks/checkout.blade.php
+++ resources/views/bigCommerce/components/blocks/checkout.blade.php
@@ -1,4 +1,4 @@
-<iframe src="{{route('index', ['store_hash' => $store_hash])}}"
+<iframe src="{{route('bigCommerce.integrations.index', ['store_hash' => $store_hash])}}"
         frameborder="0" scrolling="no" id="iFrameResizer0"
         style="overflow: hidden; width:100%; height: 1131px;">
 </iframe>
